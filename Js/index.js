1bb5fb2cea7f17f588e0bfbb86e8b35ef6485496
Eklavya-sus <eklavya0304@gmail.com>
2025-02-10 17:23:12 +0530

    js object pass by reference

---
+++ Js/index.js
@@ -0,0 +1,8 @@
+// objects are reference type 
+// hence otherRun.total = 90; will change the total of eklavyaRun object as well
+let eklavyaRun = { name:"ekavya", total:"0"};
+let otherRun = eklavyaRun;
+otherRun.total = 90;
+
+console.log(eklavyaRun);
+console.log(otherRun);
