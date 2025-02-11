4f1c990483777c3054f374d2b08db586bf18075e
Eklavya-sus <eklavya0304@gmail.com>
2025-02-11 16:06:45 +0530

    Spread operator

--- Js/index.js
+++ Js/index.js
@@ -1,7 +1,16 @@
 // objects are reference type 
 // hence otherRun.total = 90; will change the total of eklavyaRun object as well
-let eklavyaRun = { name:"ekavya", total:"0"};
-let otherRun = eklavyaRun;
+// let eklavyaRun = { name:"ekavya", total:"0"};
+// let otherRun = eklavyaRun;
+// otherRun.total = 90;
+
+// console.log(eklavyaRun);
+// console.log(otherRun);
+
+// with spread operator
+
+let eklavyaRun = { name:"ekavya"};
+let otherRun = {...eklavyaRun};
 otherRun.total = 90;
 
 console.log(eklavyaRun);
